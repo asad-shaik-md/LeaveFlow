8e49f911a03c030e39dadf85354886f5e2dd57a4
Asad Shaik <asadshaikmd@gmail.com>
2025-01-23 14:24:39 +0530

    Add bcrypt for password hashing in user registration

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,23 +1,30 @@
 import User from "../models/User.js";
 import express from "express";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
-
 const registerUser = async (req, res) => {
-    let user = await User.findOne({ employeeID: req.body.employeeID });
-    if (user) return res.status(400).send("User Already Registered!");
-    
-    user = new User({
-        employeeID: req.body.employeeID,
-        name: req.body.name,
-        password: req.body.password,
-    });
-    
-    await user.save();
-    
-    res.send(user);
+  let user = await User.findOne({ employeeID: req.body.employeeID });
+  if (user) return res.status(400).send("User Already Registered!");
+
+  user = new User({
+    employeeID: req.body.employeeID,
+    name: req.body.name,
+    password: req.body.password,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save();
+
+  res.send({
+    employeeID: user.employeeID,
+    name: user.name,
+  });
 };
 
 router.post("/", registerUser);
