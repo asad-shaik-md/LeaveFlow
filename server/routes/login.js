6422e10dfdfde45b8f7d235aa81c15d9eaf06236
Asad Shaik <asadshaikmd@gmail.com>
2025-01-28 14:33:51 +0530

    Add Joi validation for login and registration endpoints

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,11 +3,24 @@ import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
+import Joi from 'joi';
 
 const router = express.Router();
 
+function validateLogin(user) {
+  const schema = Joi.object({
+    employeeID: Joi.number().min(5).required(),
+    password: Joi.string().min(5).max(1024).required(),
+  });
+
+  return schema.validate(user)
+}
+
 const loginUser = async (req, res) => {
   try {
+    const { error } = validateLogin(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     const user = await User.findOne({ employeeID: req.body.employeeID });
     if (!user) return res.status(400).send("Invalid EmployeeID or Password.");
 
--- server/routes/register.js
+++ server/routes/register.js
@@ -2,11 +2,25 @@ import User from "../models/User.js";
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import Joi from 'joi';
 
 const router = express.Router();
 
+function validateRegister(user) {
+  const schema = Joi.object({
+    employeeID: Joi.number().min(5).required(),
+    name: Joi.string().min(5).max(155).required(),
+    password: Joi.string().min(5).max(1024).required(),
+  });
+
+  return schema.validate(user)
+}
+
 const registerUser = async (req, res) => {
   try {
+    const { error } = validateRegister(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     let user = await User.findOne({ employeeID: req.body.employeeID });
     if (user) return res.status(400).send("User Already Registered!");
 
